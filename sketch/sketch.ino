4fcab434590dc5b5e08d0638cd2c44303f768814
Quoc Tran <quoctran1998@gmail.com>
2021-09-22 21:30:35 -0700

    Added arduino code

---
+++ sketch/sketch.ino
@@ -0,0 +1,111 @@
+#define IN1 8
+#define IN2 9
+#define IN3 10
+#define IN4 11
+
+#define SWITCH 5
+
+int Steps = 0;
+boolean Direction = true;
+
+int ledState = HIGH;
+int buttonState;
+int lastButtonState = LOW; 
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  pinMode(SWITCH, INPUT);
+}
+
+void loop() {
+  while (digitalRead(SWITCH)) {
+    Serial.print(".");
+    stepper(1);
+    delayMicroseconds(800);
+  }
+  Direction = !Direction;  
+}
+
+void stepper(int xw) {
+  //Steps = 0;
+  for (int x = 0; x < xw; x++) {
+    switch (Steps) {
+      case 0:
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, HIGH);
+        break;
+      case 1:
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, HIGH);
+        break;
+      case 2:
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, LOW);
+        break;
+      case 3:
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, LOW);
+        break;
+      case 4:
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, LOW);
+        break;
+      case 5:
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, HIGH);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, LOW);
+        break;
+      case 6:
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, LOW);
+        break;
+      case 7:
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, HIGH);
+        break;
+      default:
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, LOW);
+        break;
+    }
+    SetDirection();
+  }
+}
+
+void SetDirection() {
+  if (Direction == 1) {
+    Steps++;
+  }
+  if (Direction == 0) {
+    Steps--;
+  }
+  if (Steps > 7) {
+    Steps = 0;
+  }
+  if (Steps < 0) {
+    Steps = 7;
+  }
+}
